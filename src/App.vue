b5f24cbf65a2c80c75ad5c7b63b73c672facd9de
Your NameFredyFarb <farbbone7@gmail.com>
2024-09-03 12:39:19 -0500

    proyecto vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <PrimerComponent titulo="Hola mundo en vue" 
+  nombre="Ahmad"
+  v-bind:edad="24" 
+  v-bind:src="require('@/assets/AhmadAstro.jpg')"
+  v-bind:imagenStyle="{ width: '100px', height: 'auto' }" />
+</template>
+
+<script>
+import PrimerComponent from './components/primerComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    PrimerComponent,
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/primerComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+    <h1>{{ titulo }}</h1>
+    <h1>El nombre es:
+        {{ nombre }}</h1>
+    <h1>La edad es:
+        {{ edad }}</h1>
+    <img v-bind:src="src" v-bind:alt="cosmonauta" :style="imagenStyle"/>
+    <div v-bind:style="{ color: textColor }">{{ userdata }}</div>
+    <input type="text" v-model="userdata"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PrimerComponent',
+        props: {
+            titulo: String,
+            nombre: String,
+            edad: Number,
+            src: String,
+            alt: String,
+            imagenStyle: Object
+        },
+        data() {
+            return {
+                userdata: "un valor",
+                textColor: "blue"
+            };
+        }
+    };
+</script>
+
+<style>
+
+</style>
